c365df1d200ca591271150dc9d44c15bab44c61f
Nastia2873487 <vinnikova025@gmail.com>
2025-04-22 15:35:55 +0300

    Add files via upload

---
+++ lol.py
@@ -0,0 +1,14 @@
+sitost = 0
+happy = 0
+pet = [0, 0]
+while True:
+    print("Выберите действие:Покормить или Поиграть")
+    task = input()
+    if task == "Покормить":
+        sitost += 5
+        pet[0] += 5
+        print(pet)
+    elif task == "Поиграть":
+        pet[1] += 5
+        print(pet)
+
